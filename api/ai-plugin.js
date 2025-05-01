78b6b27c4e6348a160eb8c1f932801e7f92eb36a
Ameesh Kapoor <ameesh@esqme.com>
2025-05-01 11:05:29 -0400

    Update ai plugin format

--- api/ai-plugin.js
+++ api/ai-plugin.js
@@ -1,10 +1,7 @@
 import axios from 'axios/dist/node/axios.cjs';
 
 export default async function handler(req, res) {
-  const data = '"api": { \
-    "type": "openapi", \
-    "url": "https://v0-radical-s9.vercel.app/openapi.yaml" \
-  }'
+  const data = '{ "api": { "type": "openapi", "url": "https://v0-radical-s9.vercel.app/openapi.yaml", "has_user_authentication": false }}';
   res.setHeader('Content-Type', 'text/plain');
   res.send(data);
 }
