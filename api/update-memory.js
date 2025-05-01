224ee2b06db8576e21f19498c8bb66cf26833caa
walterreid <walterreid@gmail.com>
2025-05-01 08:07:13 -0400

    Update update-memory.js
    added bearer token language

--- api/update-memory.js
+++ api/update-memory.js
@@ -36,8 +36,8 @@ export default async function handler(req, res) {
       payload,
       {
         headers: {
-          Authorization: `token ${GITHUB_TOKEN}`,
-          Accept: 'application/vnd.github.v3+json'
+          Authorization: `Bearer ${GITHUB_TOKEN}`,
+          Accept: 'application/vnd.github+json'
         }
       }
     );
